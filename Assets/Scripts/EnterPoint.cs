8413a76694abab85f8383d992335494f58bb050b
ManificHroi <nikolaev.2000.serzh@gmail.com>
2024-07-08 01:54:16 +0300

    private float _timeScale = 5

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -6,17 +6,11 @@ using Model.Config;
 using UnityEngine;
 using Utilities;
 
-//Hi, I was here!
-/*/
- * da
- * ya
-/*/
-
 public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     
     void Start()
     {
