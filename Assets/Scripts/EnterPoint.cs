9ab7c70bf96e8be512b1fc51f6ac367dfe581364
ManificHroi <nikolaev.2000.serzh@gmail.com>
2024-07-07 23:18:34 +0300

    comment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -6,6 +6,12 @@ using Model.Config;
 using UnityEngine;
 using Utilities;
 
+//Hi, I was here!
+/*/
+ * da
+ * ya
+/*/
+
 public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
